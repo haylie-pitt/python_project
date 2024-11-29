0dfd06c4eb8070b261e903424436ed6d6d82ed33
haylie-pitt <htc8@pitt.edu>
2024-11-29 11:02:45 -0500

    settings, migrations

--- flask_project/app/models.py
+++ flask_project/app/models.py
@@ -6,8 +6,8 @@ class Account(db.Model, UserMixin):  # Add UserMixin here
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    #first_name = db.Column(db.String(100), nullable=True)  # Added first name
-    #last_name = db.Column(db.String(100), nullable=True)  # Added last name
+    first_name = db.Column(db.String(100), nullable=True)  # Added first name
+    last_name = db.Column(db.String(100), nullable=True)  # Added last name
     desc = db.Column(db.String(255))
     hobbies = db.Column(db.String(255))
     age = db.Column(db.String(20))
