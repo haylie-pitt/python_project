e6312c67f2842ac2edb2b2d8bb30c97a2874e074
Haylie Carter <htc8@pitt.edu>
2024-12-03 16:57:47 -0500

    Delete flask_project-main directory

--- flask_project-main/flask_project/app/scripts/setup_db.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, db
-from app.models import Account, Event, EventAttendance  # Ensure models are imported
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()  # This initializes the tables
-    print("Database initialized!")
